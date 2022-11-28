fcba84b7e08a7c7f430c481fb42813ac32e52836
Vitalii Blyskun <97634483+VitaliiBlyskun@users.noreply.github.com>
2022-11-28 18:26:50 +0200

    show to mentor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { nanoid } from 'nanoid';
 import React, { Component } from 'react';
 import ContactForm from './ContactForm/ContactForm';
-import ContactList from './ContactList/ContactList';
+import ContactList from './ContactsList/ContactList';
 import Filter from './Filter/Filter';
 
 
